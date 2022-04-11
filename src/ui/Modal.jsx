7cce1c9b579201c35932f8a89eaa7f4895a3df47
Ricky Aston <astonmedia@hotmail.com>
2022-04-12 09:25:39 +1000

    end of section 9, Refs and portals

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,22 +1,44 @@
 import styles from "./Modal.module.css"
 import Card from "./Card"
 import Button from "./Button"
+import ReactDOM from "react-dom"
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.click} />
+}
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button click={props.click} name={"Close"} />
+      </footer>
+    </Card>
+  )
+}
 
 const Modal = (props) => {
   return (
-    <div className={styles.backdrop} onClick={props.click}>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button click={props.click} name={"Close"} />
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop click={props.click} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          click={props.click}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </>
   )
 }
 
--- src/users/CreateUserForm.jsx
+++ src/users/CreateUserForm.jsx
@@ -2,16 +2,22 @@ import styles from "./CreateUserForm.module.css"
 import Card from "../ui/Card"
 import Button from "../ui/Button"
 import Modal from "../ui/Modal"
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 const CreateUserForm = (props) => {
-  const [username, setUsername] = useState("")
-  const [age, setAge] = useState("")
+  // const [username, setUsername] = useState("")
+  // const [age, setAge] = useState("")
   const [error, setError] = useState("")
   const [isError, setIsError] = useState(false)
 
+  const nameInputRef = useRef()
+  const ageInputRef = useRef()
+
   const clickHandler = (e) => {
     e.preventDefault()
+    const username = nameInputRef.current.value
+    const age = ageInputRef.current.value
+
     if (username.trim().length <= 3) {
       setError("Please enter a username of more than 3 characters")
       setIsError(true)
@@ -23,15 +29,18 @@ const CreateUserForm = (props) => {
       return
     }
     props.addUser({ username, age })
-    setUsername("")
-    setAge("")
+    nameInputRef.current.value = ""
+    ageInputRef.current.value = ""
+    setError("")
+    setIsError(false)
   }
 
   const errorHandler = () => {
+    setError("")
     setIsError(false)
   }
   return (
-    <div>
+    <>
       {isError && (
         <Modal click={errorHandler} title={"Error Message"} message={error} />
       )}
@@ -41,20 +50,22 @@ const CreateUserForm = (props) => {
             <label htmlFor='username'>Username</label>
             <input
               type='text'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              id='username'
+              //onChange={(e) => setUsername(e.target.value)}
+              ref={nameInputRef}
             />
             <label htmlFor='age'>Age (Years)</label>
             <input
-              type='text'
-              value={age}
-              onChange={(e) => setAge(e.target.value)}
+              type='number'
+              id='age'
+              //onChange={(e) => setAge(e.target.value)}
+              ref={ageInputRef}
             />
           </div>
           <Button type={"submit"} name={"Add User"} click={clickHandler} />
         </form>
       </Card>
-    </div>
+    </>
   )
 }
 
